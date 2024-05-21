d4ffbdb46c76647d2615e1af871e9913f448e2d3
LostoVayne <moises.barrera.dev@gmail.com>
2024-05-21 01:20:47 -0400

    initial components

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#028090',
+				secondary: '#00BFB2',
+				textPrimary:"#183B56"
+			}
+		},
+	},
+	plugins: [],
+}
