3d2531e4d26d51d487f8e6617fbd9566e33f151a
casadjes <esmilcasado901@gmail.com>
2024-05-22 18:24:55 -0400

    Features Section almost complete, needs some adjustments.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#028090',
-				secondary: '#00BFB2',
-				textPrimary:"#183B56"
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#028090",
+        secondary: "#00BFB2",
+        textPrimary: "#183B56",
+        textSecondary: "#5A7184",
+      },
+    },
+  },
+  plugins: [],
+};
