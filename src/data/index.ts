d37f7954a015472ee594bd302408e25800a9729c
LostoVayne <moises.barrera.dev@gmail.com>
2024-06-01 17:44:48 -0400

    mejorado el carrousel

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,10 +1,59 @@
-interface Testimonial {
+type Testimonial = Array<{ title: string; description: string; rating?: number }>;
 
-}
+export const TestimonialData: Testimonial = [
+  {
+    title: "Testimonial 1",
+    description: "Testimonial 1 description",
+  },
+  {
+    title: "Testimonial 2",
+    description: "Testimonial 2 description",
+  },
+  {
+    title: "Testimonial 3",
+    description: "Testimonial 3 description",
+  },
+  {
+    title: "Testimonial 4",
+    description: "Testimonial 4 description",
+  },
+];
 
+export const Services = [
+  {
+    name: "How It's Works",
+    href: "/",
+  },
+  {
+    name: "Features",
+    href: "/",
+  },
+  {
+    name: "Integrations",
+    href: "/",
+  },
+  {
+    name: "Pricing",
+    href: "/",
+  },
+];
 
-const TestimonialData : Testimonial = [
-    {
+ export const Company = [
+  {
+    name: "About",
+    href: "/",
+  },
 
-    }
-]
+  {
+    name: "Terms",
+    href: "/",
+  },
+  {
+    name: "Privacy Policy",
+    href: "/",
+  },
+  {
+    name: "Careers",
+    href: "/",
+  },
+];
